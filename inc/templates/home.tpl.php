22b7d5ce9e5c2d960013ec638a3ad000ed31ff34
kadev-oclock <karimaoudiaa@gmail.com>
2020-11-10 14:42:42 +0100

    first commit

---
+++ inc/templates/home.tpl.php
@@ -0,0 +1,77 @@
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-md-12">
+                <!-- emmet : (div.card>div.card-header>lorem5^div.card-body>lorem40^div)*4 -->
+                <div class="card mb-4">
+                    <div class="card-header"><a href="article.php">Lorem ipsum dolor sit amet.</a></div>
+                    <div class="card-body">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati repellat
+                        molestiae voluptas tempore consequatur, quas laudantium fugiat odio fugit ut modi aliquam sed
+                        enim
+                        culpa quibusdam. Praesentium vitae dignissimos nesciunt reiciendis? Veritatis error mollitia
+                        explicabo ipsam est tempora et suscipit.</div>
+                    <div></div>
+                </div>
+                <div class="card mb-4">
+                    <div class="card-header"><a href="article.php">Ratione ipsum iusto atque. Ipsa.</a></div>
+                    <div class="card-body">Accusantium eum natus explicabo, totam magni culpa mollitia at perspiciatis
+                        inventore est. Rerum nisi earum mollitia delectus expedita commodi natus inventore eos! Maxime
+                        libero quas soluta, suscipit tenetur nesciunt quod iure consectetur delectus ea. Excepturi
+                        veritatis
+                        laudantium culpa quidem minima!</div>
+                    <div></div>
+                </div>
+                <div class="card mb-4">
+                    <div class="card-header"><a href="article.php">Veniam minima aliquam aspernatur quasi!</a></div>
+                    <div class="card-body">Accusamus eius eum iste sit officiis quia animi aliquid qui, vero, nihil
+                        exercitationem nemo nulla dignissimos unde praesentium enim, porro est? Obcaecati atque at,
+                        suscipit
+                        in itaque dicta illo magni sed consequuntur! Distinctio voluptatem iure fuga vitae explicabo
+                        ipsam
+                        architecto.</div>
+                    <div></div>
+                </div>
+                <div class="card mb-4">
+                    <div class="card-header"><a href="article.php">Debitis eius repudiandae quo veniam.</a></div>
+                    <div class="card-body">Eum sed temporibus sequi quod cumque repellendus fugit aspernatur eaque modi
+                        excepturi nesciunt, fugiat mollitia quae, harum aut saepe facilis! Voluptates, iste illum esse
+                        magnam odio rem laborum debitis possimus expedita ipsam perspiciatis, doloribus aut corrupti
+                        accusantium, omnis aliquam quaerat.</div>
+                    <div></div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-12">
+                <form class="form">
+                    <div class="form-row">
+                        <div class="col">
+                            <label for="search" class="sr-only">Rechercher</label>
+                            <input type="text" class="form-control mr-2" id="search">
+                        </div>
+                        <div class="col-4">
+                            <button type="submit" class="btn btn-primary">Rechercher</button>
+                        </div>
+                    </div>
+                </form>
+                <!-- emmet : (div.card>div.card-header+ul.list-group.list-group-flush>li.list-group-item*4)*2 -->
+                <div class="card mb-4">
+                    <div class="card-header">Catégories</div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item"><a href="category.php">Teamback</a></li>
+                        <li class="list-group-item"><a href="category.php">Teamfront</a></li>
+                        <li class="list-group-item"><a href="category.php">Collaboration</a></li>
+                        <li class="list-group-item"><a href="category.php">Ma vie de dev</a></li>
+                    </ul>
+                </div>
+                <div class="card mb-4">
+                    <div class="card-header">Auteurs</div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item"><a href="author.php">Maxime</a></li>
+                        <li class="list-group-item"><a href="author.php">Dario</a></li>
+                        <li class="list-group-item"><a href="author.php">Lucie</a></li>
+                        <li class="list-group-item"><a href="author.php">Anthony</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
